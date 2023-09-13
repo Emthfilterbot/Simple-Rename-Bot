961e6ab30696268552cc6e2fd0a743d53977eccb
Jijo <96016542+Emthfilterbot@users.noreply.github.com>
2023-09-13 19:29:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
+API_ID = int(environ.get("API_ID", "18368023"))
+API_HASH = environ.get("API_HASH", "10afacc8dc64d004422513027acf38d5")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6562958250:AAH6ZbZnDdrn88HUU0sOKNSmdX1fAOSeNXQ")
+ADMIN = int(environ.get("ADMIN", "696471140"))          
 CAPTION = environ.get("CAPTION", "")
 
 class temp(object):
